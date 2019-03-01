2bd466d45ee6f49205049101d7c34cd5dcf50f9e
Leif Johansson <leifj@sunet.se>
2019-03-01 08:50:48 +0100

    cleanup templates

--- src/jquery-ds-widget.js
+++ src/jquery-ds-widget.js
@@ -28,13 +28,13 @@ jQuery(function ($) {
             let obj = this;
             if (typeof obj.options['render'] !== 'function') {
                 obj._template_with_icon = Hogan.compile('<div data-href="{{entity_id}}" class="identityprovider list-group-item">' +
-                    '{{^sticky}}<button type="button" alt="{{ _(\"Remove from list\") }}" data-toggle="tooltip" data-placement="left" class="remove close">&times;</button>{{/sticky}}' +
+                    '{{^sticky}}<button type="button" alt="Remove from list" data-toggle="tooltip" data-placement="left" class="remove close">&times;</button>{{/sticky}}' +
                     '<div class="media"><div class="d-flex mr-3"><div class="frame-round">' +
                     '<div class="crop"><img{{#entity_icon}} src="{{entity_icon}}"{{/entity_icon}} data-id={{entity_id}} class="pyff-idp-icon"/></div></div></div>' +
                     '<div class="media-body"><h5 class="mt-0 mb-1">{{title}}</h5>{{#descr}}{{descr}}{{/descr}}</div>' +
                     '</div></div>');
                 obj._template_no_icon = Hogan.compile('<div data-href="{{entity_id}}" class="identityprovider list-group-item">' +
-                    '{{^sticky}}<button type="button" alt="{{ _(\"Remove from list\") }}" data-toggle="tooltip" data-placement="left" class="remove close">&times;</button>{{/sticky}}' +
+                    '{{^sticky}}<button type="button" alt="Remove from list" data-toggle="tooltip" data-placement="left" class="remove close">&times;</button>{{/sticky}}' +
                     '<div class="media"><div class="d-flex mr-3"><div class="frame-round" style="visibility: hidden;">' +
                     '<div class="crop"><img{{#entity_icon}} src="{{entity_icon}}"{{/entity_icon}} data-id={{entity_id}} class="pyff-idp-icon"/></div></div></div>' +
                     '<div class="media-body"><h5 class="mt-0 mb-1">{{title}}</h5>{{#descr}}{{descr}}{{/descr}}</div>' +
