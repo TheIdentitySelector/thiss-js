aa672c3beb83c62ff6287093bdc8e85ff62f148d
Leif Johansson <leifj@sunet.se>
2020-03-10 08:37:43 +0100

    clarify copyright

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@
 # -- Project information -----------------------------------------------------
 
 project = u'The Identity Selector Service'
-copyright = u'2019, Leif Johansson'
+copyright = u'2019, Swedish University Network (SUNET)'
 author = u'Leif Johansson'
 
 # The short X.Y version
