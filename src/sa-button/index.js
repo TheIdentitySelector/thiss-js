586bc06f99a9df83a78ed38787f513ce2143b0a8
Robert Haidari <robert.haidari@hotemu.com>
2020-10-25 11:26:03 -0500

    Added lover text under the button.

--- src/sa-button/index.js
+++ src/sa-button/index.js
@@ -1,2 +1,7 @@
 import '../assets/sa-button.scss'
 import '../assets/sa-icon.svg'
+import { dom, library } from '@fortawesome/fontawesome-svg-core'
+import { faPen } from '@fortawesome/free-solid-svg-icons/faPen'
+
+library.add( faPen );
+dom.watch();
