143dfa6d35878fc8f59e42130bb05af123ea1fc8
Robert Haidari <robert.haidari@hotemu.com>
2021-06-11 11:54:46 -0500

    Corrected setting IFrame attributes.

--- src/component.js
+++ src/component.js
@@ -53,8 +53,9 @@ function prerenderTemplate(opts) {
 function containerTemplate(opts) {
     _set_default_props(opts);
 
-    opts.frame.title = 'SeamlessAccess Button'
-    opts.frame.ariaLabel = 'SeamlessAccess Button'
+    opts.frame.setAttribute('title', 'SeamlessAccess Button')
+    opts.frame.setAttribute('aria-label', 'SeamlessAccess Button')
+    opts.frame.setAttribute('role', 'presentation')
 
     let uid = opts.uid;
     let doc = opts.doc;
