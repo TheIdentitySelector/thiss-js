e5a74e50412b899d677b4aba5ac998a623810405
Robert Haidari <robert.haidari@hotemu.com>
2021-08-06 08:59:49 -0500

    Localization feature.

--- src/ds/index.js
+++ src/ds/index.js
@@ -1,6 +1,7 @@
 import '../assets/nc.scss';
 import '../assets/ds.scss';
 
+import '../localization.js'
 
 import { dom, library, config } from '@fortawesome/fontawesome-svg-core';
 import {faPlusSquare} from '@fortawesome/free-solid-svg-icons/faPlusSquare';
---
+++ src/localization.js
@@ -0,0 +1,51 @@
+const I18n = require('banana-i18n/dist/banana-i18n.js');
+
+const DEFAULT_LANGUAGE = 'en-US'
+const I18N_DATA_ATTRIBUTE = '[data-i18n]'
+
+const locale = window.navigator.userLanguage || window.navigator.language;
+const languageSelector = document.getElementById('language-selector')
+const i18n = new I18n()
+
+const setLanguage = (language) => {
+    i18n.setLocale(language);
+
+    fetch(`/translations/${language}.json`)
+        .then((response) => {
+            return response.json();
+        }).then((messages) => {
+            i18n.setLocale(language);
+            i18n.load(messages, language);
+
+            const allElements = document.querySelectorAll(I18N_DATA_ATTRIBUTE)
+
+            for (let i = 0; i < allElements.length; i++) {
+                const dataValue = allElements[i].dataset.i18n;
+
+                if (dataValue) {
+                    const string = i18n.i18n(dataValue)
+
+                    if (string && string !== dataValue) {
+                        allElements[i].textContent = string
+                    }
+                }
+            }
+        })
+        .catch((error) => {
+            console.log('error: ', error)
+            setLanguage(DEFAULT_LANGUAGE)
+        })
+}
+
+const setLanguageSelector = (language) => {
+    languageSelector.value = language
+}
+
+setLanguage(locale)
+setLanguageSelector(locale)
+
+if (languageSelector) {
+    languageSelector.addEventListener('change', (event) => {
+        setLanguage(event.target.value)
+    });
+}
