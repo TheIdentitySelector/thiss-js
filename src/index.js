65fafa6c7a2ea10f848b22db442d4dd62b31fc71
Robert Haidari <robert.haidari@hotemu.com>
2021-10-20 16:40:10 -0500

    Removed auto default to english for cta, cta defaults to browser language, fixed dropdown styling"

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 window.onload = function() {
     DiscoveryComponent({
        loginHandlerURL: process.env.BASE_URL+'ds/?target=https://google.com&return=/&entityID=https://ra21.mnt.se/shibboleth',
-       backgroundColor: '#e9ecef',
-       locale: 'es-ES'
+       backgroundColor: '#e9ecef'
     }).render('#login');
 };
