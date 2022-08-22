9734c94fa89cdf2c606816471779312f0c92b587
Leif Johansson <leifj@sunet.se>
2022-08-22 10:23:32 +0200

    update version in docs

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = u'Leif Johansson'
 # The short X.Y version
 version = u'1.0'
 # The full version, including alpha/beta/rc tags
-release = u'1.6.2'
+release = u'1.6.3'
 js_source_path = '../src'
 
 # -- General configuration ---------------------------------------------------
