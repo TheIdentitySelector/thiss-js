a968cff36f8a9ef66294bbb2980ec398e5294fc4
enriquepablo <enrique@cazalla.net>
2024-09-06 12:05:42 +0200

    positive hints, wip

--- src/result/index.js
+++ src/result/index.js
@@ -1,7 +1,5 @@
-import {DiscoveryComponent} from "./component"; /* webpackPrefetch: true */
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/index.scss';
-import './assets/sa-black.svg'
 
 window.onload = function() {
     const entityID = urlParams.get('entityID');
