f089d8d376de14f04c274dbfec5aafd259791ae7
enriquepablo <enrique@cazalla.net>
2024-09-06 12:57:45 +0200

    positive hints, wip

--- src/index.js
+++ src/index.js
@@ -8,12 +8,72 @@ window.onload = function() {
         loginHandlerURL: process.env.BASE_URL+`ds/?target=https://google.com&return=${process.env.BASE_URL}result`,
         backgroundColor: '#e9ecef',
         entityID: 'https://cpauth.icos-cp.eu/saml/cpauth',
-        trustProfile: 'customer'
+        trustProfile: 'global1'
     }).render('#login1');
     DiscoveryComponent({
         loginHandlerURL: process.env.BASE_URL+`ds/?target=https://google.com&return=${process.env.BASE_URL}result`,
         backgroundColor: '#e9ecef',
         entityID: 'https://cpauth.icos-cp.eu/saml/cpauth',
-        trustProfile: 'other'
+        trustProfile: 'provider'
     }).render('#login2');
+    DiscoveryComponent({
+        loginHandlerURL: process.env.BASE_URL+`ds/?target=https://google.com&return=${process.env.BASE_URL}result`,
+        backgroundColor: '#e9ecef',
+        entityID: 'https://cpauth.icos-cp.eu/saml/cpauth',
+        trustProfile: 'other'
+    }).render('#login3');
+    DiscoveryComponent({
+        loginHandlerURL: process.env.BASE_URL+`ds/?target=https://google.com&return=${process.env.BASE_URL}result`,
+        backgroundColor: '#e9ecef',
+        entityID: 'https://cpauth.icos-cp.eu/saml/cpauth',
+        trustProfile: 'customer'
+    }).render('#login4');
+    DiscoveryComponent({
+        loginHandlerURL: process.env.BASE_URL+`ds/?target=https://google.com&return=${process.env.BASE_URL}result`,
+        backgroundColor: '#e9ecef',
+        entityID: 'https://test-edusign.ed-integrations.com/shibboleth',
+        trustProfile: 'customer2'
+    }).render('#login5');
+    DiscoveryComponent({
+        loginHandlerURL: process.env.BASE_URL+`ds/?target=https://google.com&return=${process.env.BASE_URL}result`,
+        backgroundColor: '#e9ecef',
+        entityID: 'https://test-edusign.ed-integrations.com/shibboleth',
+        trustProfile: 'customer3'
+    }).render('#login6');
+    DiscoveryComponent({
+        loginHandlerURL: process.env.BASE_URL+`ds/?target=https://google.com&return=${process.env.BASE_URL}result`,
+        backgroundColor: '#e9ecef',
+        entityID: 'https://test-edusign.ed-integrations.com/shibboleth',
+        trustProfile: 'customer4'
+    }).render('#login7');
+    DiscoveryComponent({
+        loginHandlerURL: process.env.BASE_URL+`ds/?target=https://google.com&return=${process.env.BASE_URL}result`,
+        backgroundColor: '#e9ecef',
+        entityID: 'https://box-idp.nordu.net/simplesaml/module.php/saml/sp/metadata.php/default-sp',
+        trustProfile: 'customer'
+    }).render('#login8');
+    DiscoveryComponent({
+        loginHandlerURL: process.env.BASE_URL+`ds/?target=https://google.com&return=${process.env.BASE_URL}result`,
+        backgroundColor: '#e9ecef',
+        entityID: 'https://box-idp.nordu.net/simplesaml/module.php/saml/sp/metadata.php/default-sp',
+        trustProfile: 'customer2'
+    }).render('#login9');
+    DiscoveryComponent({
+        loginHandlerURL: process.env.BASE_URL+`ds/?target=https://google.com&return=${process.env.BASE_URL}result`,
+        backgroundColor: '#e9ecef',
+        entityID: 'https://box-idp.nordu.net/simplesaml/module.php/saml/sp/metadata.php/default-sp',
+        trustProfile: 'customer3'
+    }).render('#login10');
+    DiscoveryComponent({
+        loginHandlerURL: process.env.BASE_URL+`ds/?target=https://google.com&return=${process.env.BASE_URL}result`,
+        backgroundColor: '#e9ecef',
+        entityID: 'https://box-idp.nordu.net/simplesaml/module.php/saml/sp/metadata.php/default-sp',
+        trustProfile: 'customer4'
+    }).render('#login11');
+    DiscoveryComponent({
+        loginHandlerURL: process.env.BASE_URL+`ds/?target=https://google.com&return=${process.env.BASE_URL}result`,
+        backgroundColor: '#e9ecef',
+        entityID: 'https://csucoast.infoready4.com/shibboleth',
+        trustProfile: 'customer'
+    }).render('#login12');
 };
