ac352466c15ae7c13482109b271aa40ae509bd6b
enriquepablo <enrique@cazalla.net>
2024-09-06 13:03:15 +0200

    positive hints, wip

--- src/result/index.js
+++ src/result/index.js
@@ -1,9 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-window.onload = function() {
+$(document).ready(function() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const entityID = urlParams.get('entityID');
     $("#choice").text(entityID)
-};
+});
 
