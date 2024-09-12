c7402d63a7fed123efc4a196593998bec6092181
enriquepablo <enrique@cazalla.net>
2024-09-12 18:04:43 +0200

    add trust profile for inCommon to netlify app

--- src/index.js
+++ src/index.js
@@ -14,6 +14,13 @@ window.onload = function() {
         loginHandlerURL: process.env.BASE_URL+`ds/?target=https://google.com&return=${process.env.BASE_URL}result`,
         backgroundColor: '#e9ecef',
         entityID: 'https://cpauth.icos-cp.eu/saml/cpauth',
+        trustProfile: 'incommon-wayfinder',
+        discoveryResponse: `${process.env.BASE_URL}result`
+    }).render('#login05');
+    DiscoveryComponent({
+        loginHandlerURL: process.env.BASE_URL+`ds/?target=https://google.com&return=${process.env.BASE_URL}result`,
+        backgroundColor: '#e9ecef',
+        entityID: 'https://cpauth.icos-cp.eu/saml/cpauth',
         trustProfile: 'global1',
         discoveryResponse: `${process.env.BASE_URL}result`
     }).render('#login1');
