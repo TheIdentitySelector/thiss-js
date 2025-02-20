d85093d3a91b5eb9bf3f6d36edd2942467888c1e
enriquepablo <enrique@cazalla.net>
2025-02-20 12:16:22 +0100

    fix pinned entities for firefox

--- src/ps/index.js
+++ src/ps/index.js
@@ -306,7 +306,11 @@ try {
     await initCheckbox();
     postRobot.send(window.parent, 'initialized')
            .catch(err => {
-               console.log(`No intialized handler: ${err}`)
+               console.log(`No intialized handler`)
+           });
+    postRobot.send(window.parent, 'initialized-entity')
+           .catch(err => {
+               console.log(`No intialized-entity handler`)
            });
 } catch (err) {
     console.log(`Problem initializing client: ${err}`);
