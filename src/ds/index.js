cecd5c40bc237b709b193469da4d2e343b5296eb
enriquepablo <enrique@cazalla.net>
2025-03-04 19:21:12 +0100

    render choices in more robust manner

--- src/ds/index.js
+++ src/ds/index.js
@@ -209,17 +209,21 @@ $(document).ready(function() {
                 clearTimeout(timer); timer = null;
             }
 
-            json_mdq_get_sp(entityID, mdq_url).then(spEntity => {
-                let strict = true;
-                if (trustProfile && 'tinfo' in spEntity && 
-                          'profiles' in spEntity.tinfo &&
-                           trustProfile in spEntity.tinfo.profiles)
-                    strict = spEntity.tinfo.profiles[trustProfile].strict;
+            try {
+                json_mdq_get_sp(entityID, mdq_url).then(spEntity => {
+                    let strict = true;
+                    if (trustProfile && 'tinfo' in spEntity && 
+                              'profiles' in spEntity.tinfo &&
+                               trustProfile in spEntity.tinfo.profiles)
+                        strict = spEntity.tinfo.profiles[trustProfile].strict;
 
-                self._render_search_result(items, strict, spEntity);
-            }).catch(err => {
+                    self._render_search_result(items, strict, spEntity);
+                }).catch(err => {
+                    self._render_search_result(items, true, null);
+                });
+            } catch (err) {
                 self._render_search_result(items, true, null);
-            });
+            }
         },
         _render_saved_choice: function(items, strict, spEntity) {
 
@@ -287,18 +291,20 @@ $(document).ready(function() {
                 clearTimeout(timer); timer = null;
             }
 
-            json_mdq_get_sp(entityID, mdq_url).then(spEntity => {
-                let strict = true;
-                if (trustProfile && 'tinfo' in spEntity && 
-                          'profiles' in spEntity.tinfo &&
-                           trustProfile in spEntity.tinfo.profiles) {
-                    strict = spEntity.tinfo.profiles[trustProfile].strict;
-                }
-                
-                self._render_saved_choice(items, strict, spEntity);
-            }).catch(err => {
-                self._render_saved_choice(items, true, null);
-            });
+            if (entityID) {
+                json_mdq_get_sp(entityID, mdq_url).then(spEntity => {
+                    let strict = true;
+                    if (trustProfile && 'tinfo' in spEntity && 
+                              'profiles' in spEntity.tinfo &&
+                               trustProfile in spEntity.tinfo.profiles) {
+                        strict = spEntity.tinfo.profiles[trustProfile].strict;
+                    }
+                    
+                    self._render_saved_choice(items, strict, spEntity);
+                }).catch(err => {
+                    self._render_saved_choice(items, true, null);
+                });
+            }
         },
         too_many_results: function(bts, count) {
             $("#searching").addClass('d-none');
