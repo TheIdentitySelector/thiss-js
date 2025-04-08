7f469fc6078bb9298332cd7f6ed26a3b60878264
enriquepablo <enrique@cazalla.net>
2025-04-08 16:13:55 +0200

    bump version to 2.1.55

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = u'Leif Johansson'
 # The short X.Y version
 version = u'2.1'
 # The full version, including alpha/beta/rc tags
-release = u'2.1.54'
+release = u'2.1.55'
 js_source_path = '../src'
 
 # -- General configuration ---------------------------------------------------
