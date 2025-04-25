7a068dd92956fbbb64596c37daf8549ac90b33af
enriquepablo <enrique@cazalla.net>
2025-04-25 12:01:54 +0200

    fixing netlify preview

--- src/cta/index.js
+++ src/cta/index.js
@@ -9,7 +9,7 @@ library.add(faExternalLink);
 dom.watch();
 
 import {ds_response_url, json_mdq_pre_get, DiscoveryService} from "@theidentityselector/thiss-ds/src/discovery.js";
-import {requestingStorageAccess, hasSAPerm} from "../storage/index.js";
+import {requestingStorageAccess} from "../storage/index.js";
 import hex_sha1 from '@theidentityselector/thiss-ds/src/sha1.js';
 
 import {DiscoveryComponent} from "../component";
