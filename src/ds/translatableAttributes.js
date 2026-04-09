4e24b87ca535ef8c56c89be7026241b204389fda
Enrique Pérez Arnaud <enrique@cazalla.net>
2026-04-09 18:06:33 +0200

    translate some html attributes

---
+++ src/ds/translatableAttributes.js
@@ -0,0 +1,10 @@
+
+
+export const translatable = [
+    ["aria-label-searchinput", "#searchinput", "aria-label"],
+    ["aria-label-remove-institute", "#remove-saved-institute", "aria-label"],
+    ["aria-label-remove-institute", "#remove-suggested-institute", "aria-label"],
+    ["aria-label-close-info-banner", "#close-info-banner", "aria-label"],
+    ["aria-label-locale-selector", "#locale-selector", "aria-label"],
+    ["title-remove-institution", ".institution-remove-cross-wrapper", "title"],
+];
