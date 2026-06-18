6d052218fdb3ca8b90619898c69c4e2952508380
Enrique Pérez Arnaud <enrique@cazalla.net>
2026-06-18 16:45:24 +0200

    bump to 2.1.188

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
-VERSION:=2.1.187
+VERSION:=2.1.188
 PREV_VERSION:=2.1.179
-PRE_RELEASE:=false
+PRE_RELEASE:=true
 PWD:=$(shell pwd)
 NAME:=thiss-js
 REGISTRY:=docker.sunet.se
--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = u'Leif Johansson'
 # The short X.Y version
 version = u'2.1'
 # The full version, including alpha/beta/rc tags
-release = u'2.1.187'
+release = u'2.1.188'
 js_source_path = '../src'
 
 # -- General configuration ---------------------------------------------------
